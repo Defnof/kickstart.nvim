9dd541eb8938bb8f0a3a907f667a268631257541
Zsolt Takacs <jtamer28@gmail.com>
2024-02-17 19:07:42 +0200

    Added alpha and session-manager

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,4 +2,36 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local config = require 'alpha.themes.dashboard'.config
+      require 'alpha'.setup(config)
+    end
+  },
+  {
+    "Shatur/neovim-session-manager",
+    config = function(opts)
+      local session_manager = require('session_manager')
+      -- Auto save session
+      vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+        callback = function()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            -- Don't save while there's any 'nofile' buffer open.
+            if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
+              return
+            end
+          end
+          session_manager.save_current_session()
+        end
+      })
+
+
+      opts['autoload_mode'] = require('session_manager.config').AutoloadMode.Disabled
+
+      session_manager.setup(opts)
+    end,
+  }
+}
