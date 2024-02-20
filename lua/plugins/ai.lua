32e6c3e1c7836c48bdc25ffde9faa07aa96da7f9
Zsolt Takacs <jtamer28@gmail.com>
2024-02-20 16:37:27 +0200

    Added AI plugins

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "piersolenski/wtf.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {},
+    keys = {
+      {
+        "gw",
+        mode = { "n", "x" },
+        function()
+          require("wtf").ai()
+        end,
+        desc = "Debug diagnostic with AI",
+      },
+      {
+        "gW",
+        mode = { "n" },
+        function()
+          require("wtf").search()
+        end,
+        desc = "Search diagnostic with Google",
+      },
+    },
+  }
+}
