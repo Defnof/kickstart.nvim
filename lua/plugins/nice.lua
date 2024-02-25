a2ca51f7eb577ed0a2a6676bd36c458cbe92c44a
Zsolt Takacs <jtamer28@gmail.com>
2024-02-25 13:10:40 +0200

    Remove hydrate

--- lua/plugins/nice.lua
+++ lua/plugins/nice.lua
@@ -167,6 +167,7 @@ return {
 
   {
     'stefanlogue/hydrate.nvim',
+    enabled = false,
     -- This installs the latest stable release.
     -- Set to false or omit to install the latest development version
     version = '*',
