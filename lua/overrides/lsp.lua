8826baad57f6573289cc64cf79fe0f9aa734b876
Zsolt Takacs <jtamer28@gmail.com>
2024-02-28 20:57:05 +0200

    Revamp lspconfig based on kickstart and nvchad config

--- lua/overrides/lsp.lua
+++ lua/overrides/lsp.lua
@@ -4,79 +4,11 @@ local map_lsp_keybinds = require('utils.mapping').map_lsp_keybinds
 local on_attach = function(client, bufnr)
   return map_lsp_keybinds(bufnr)
 end -- mason-lspconfig requires that these setup functions are called in this order
+
 -- before setting up the servers.
 require('mason').setup()
-require('mason-lspconfig').setup()
-
--- NOTE: TSServer methods
-local function execute_ts_command(command)
-  return function()
-    local params = {
-      command = '_typescript.' .. command,
-      arguments = {
-        vim.api.nvim_buf_get_name(0),
-      },
-    }
-
-    vim.lsp.buf.execute_command(params)
-  end
-end
 
 local lsputils = require 'lspconfig.util'
-
-local tsserver = {
-  on_attach = function(client, bufnr)
-    -- require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
-    map_lsp_keybinds(bufnr)
-  end,
-  -- NOTE: Settings for inlay hints for JS and TS
-  settings = {
-    typescript = {
-      inlayHints = {
-        includeInlayParameterNameHints = 'all',
-        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-    },
-    javascript = {
-      inlayHints = {
-        includeInlayParameterNameHints = 'all',
-        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-    },
-  },
-  init_options = {
-    preferences = {
-      importModuleSpecifierPreference = 'absolute',
-      importModuleSpecifierEnding = 'minimal',
-    },
-  },
-  commands = {
-    OrganizeImports = {
-      execute_ts_command 'organizeImports',
-      description = 'Organize Imports',
-    },
-    RemoveUnusedImports = {
-      execute_ts_command 'removeUnused',
-      description = 'Remove Unused Imports',
-    },
-    FixAll = {
-      execute_ts_command 'fixAll',
-      description = 'Fix All',
-    },
-  },
-}
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -121,8 +53,6 @@ local servers = {
       'typescriptreact',
     },
   },
-  -- WARN: Deprecated, replaced by typescript-tools.nvim
-  tsserver = tsserver,
   jsonls = {
     settings = {
       json = {
@@ -134,8 +64,6 @@ local servers = {
     },
   },
   bashls = {},
-  rust_analyzer = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -153,40 +81,37 @@ require('neodev').setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-local ensure_installed = {
+-- You can add other tools here that you want Mason to install
+-- for you, so that they are available from within Neovim.
+local ensure_installed = vim.tbl_keys(servers or {})
+vim.list_extend(ensure_installed, {
   'biome',
   'codespell',
   'isort',
   'stylua',
   'shfmt',
-}
 
-vim.tbl_deep_extend('force', {}, vim.tbl_keys(servers), ensure_installed)
+  -- additional servers that we don't want to setup via lspconfig
+  'tsserver',
+  'rust_analyzer',
+})
 
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
+-- Ensure the servers above are installed
+require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    -- NOTE: Add defaults
-    local settings = servers[server_name]
+-- NOTE: For each server setup
+for server, settings in pairs(servers) do
+  if settings['on_attach'] == nil then
+    settings['on_attach'] = on_attach
+  end
 
-    if settings['on_attach'] == nil then
-      settings['on_attach'] = on_attach
-    end
+  if settings['capabilities'] == nil then
+    settings['capabilities'] = capabilities
+  end
 
-    if settings['capabilities'] == nil then
-      settings['capabilities'] = capabilities
-    end
+  if settings['filetypes'] == nil then
+    settings['filetypes'] = servers[server].filetypes
+  end
 
-    if settings['filetypes'] == nil then
-      settings['filetypes'] = servers[server_name].filetypes
-    end
-
-    require('lspconfig')[server_name].setup(settings)
-  end,
-}
+  require('lspconfig')[server].setup(settings)
+end
