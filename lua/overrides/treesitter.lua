a26981ac5cdd1f47844fd52b7a597fed78dbe581
Zsolt Takacs <jtamer28@gmail.com>
2024-02-29 10:11:24 +0200

    Remove tree-sitter autoinstall

--- lua/overrides/treesitter.lua
+++ lua/overrides/treesitter.lua
@@ -30,7 +30,7 @@ vim.defer_fn(function()
     },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = true,
+    auto_install = false,
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
     -- List of parsers to ignore installing
